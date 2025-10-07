0272f3361a9e0929d9ad1b4ac6e7c9badcb48ba6
Isabella Zhu <izzyzhunet@gmail.com>
2025-10-07 11:55:13 -0400

    modify eval script

--- eval/eval_ados.py
+++ eval/eval_ados.py
@@ -4,21 +4,74 @@
 from tools.vamba_chat import Vamba
 import json
 
-RESIZE_WIDTH, RESIZE_HEIGHT = 360, 640
+RESIZE_WIDTH, RESIZE_HEIGHT = 360, 640 # TODO picked arbitrarily
+FRAME_COUNT = 150 # TODO picked arbitrarily
+
+SEGMENTS = [
+    'anticipation_of_routine_1',
+    'anticipation_of_routine_2',
+    'anticipation_of_routine_3',
+    'anticipation_of_routine_4',
+    'anticipation_of_routine_5',
+    'anticipation_of_routine_6',
+    'anticipation_of_routine_7',
+    'bubble_play',
+    'free_play',
+    'response_to_joint_attention_1',
+    'response_to_joint_attention_2',
+    'response_to_name',
+    'responsive_social_smile_1',
+    'responsive_social_smile_2',
+    'responsive_social_smile_3',
+    'responsive_social_smile_4',
+]
+
+def get_prompts(json_obj):
+
+    labels_str = "\n".join(json_obj["labels"])
+
+    response = (
+        "system\n"
+        "You are a helpful assistant.\n"
+        "user\n"
+        f"You are analyzing an ADOS (Autism Diagnostic Observation Schedule) video with {FRAME_COUNT} frames.\n\n"
+        f"Module: {json_obj['module']}\n"
+        f"Test: {json_obj['test type']}\n"
+        f"Task: {json_obj['description']}\n\n"
+        f"Labels:\n{labels_str}\n\n"
+        "Provide your answer in a single number and nothing else."
+    )
+
+    return [
+        {
+            'module' : json_obj['module'],
+            'test_type' : json_obj['test type'],
+            'response' : response,
+            'num_frames' : FRAME_COUNT,
+            'use_aks' : True,
+            'segment' : segment
+        }
+
+        for segment in SEGMENTS
+    ]
 
 model = Vamba(model_path="TIGER-Lab/Vamba-Qwen2-VL-7B", device="cuda")
 
-with open('data/output.json', 'r') as f:
+with open('data/autism_scoring.json', 'r') as f:
     data = json.load(f)
 
 results = []
+prompts = []
 
-for idx, item in enumerate(data):
+for item in data:
+    prompts.extend(get_prompts(item))
 
-    print(f"On prompt {idx}, module {item['module']}, test_type {item['test_type']}...")
+print(f"TOTAL NUMBER OF PROMPTS: {len(prompts)}")
 
-    prompt = item['response'].split('Response')[0] # TODO fix hack
-
+for idx, item in enumerate(prompts):
+    
+    print(f"On prompt {idx}, module {item['module']}, test_type {item['test_type']}, segment {item['segment']}...")
+    prompt = item['response']
     print(prompt)
 
     test_input = [
